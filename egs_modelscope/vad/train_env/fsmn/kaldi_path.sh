dd505dd67b84eeb4138fd30f563962a78f81d7b3
lingyunfly <121302812+lingyunfly@users.noreply.github.com>
2023-05-31 19:12:38 +0800

    Update kaldi_path.sh

--- egs_modelscope/vad/train_env/fsmn/kaldi_path.sh
+++ egs_modelscope/vad/train_env/fsmn/kaldi_path.sh
@@ -1,4 +1,4 @@
-export KALDI_ROOT=/nfs/ailsa.zly/tfbase/espnet_work/kaldi/
+export KALDI_ROOT=./espnet_work/kaldi/
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
