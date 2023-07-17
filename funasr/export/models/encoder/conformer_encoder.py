32976ef624fb37cd23b1d367d4b8bfa624bf1cdc
nichongjia-2007 <nichongjia@gmail.com>
2023-07-17 14:05:28 +0800

    modify conformer encoder

--- funasr/export/models/encoder/conformer_encoder.py
+++ funasr/export/models/encoder/conformer_encoder.py
@@ -13,7 +13,7 @@ from funasr.export.models.encoder.sanm_encoder import SANMEncoder
 from funasr.modules.attention import RelPositionMultiHeadedAttention
 # from funasr.export.models.modules.multihead_att import RelPositionMultiHeadedAttention as RelPositionMultiHeadedAttention_export
 from funasr.export.models.modules.multihead_att import OnnxRelPosMultiHeadedAttention as RelPositionMultiHeadedAttention_export
-
+from funasr.export.models.language_models.embed import Embedding
 
 class ConformerEncoder(nn.Module):
     def __init__(
@@ -25,7 +25,7 @@ class ConformerEncoder(nn.Module):
         onnx: bool = True,
     ):
         super().__init__()
-        self.embed = model.embed
+        self.embed = Embedding(model.embed, max_seq_len)
         self.model = model
         self.feats_dim = feats_dim
         self._output_size = model._output_size
@@ -48,7 +48,6 @@ class ConformerEncoder(nn.Module):
         self.num_heads = model.encoders[0].self_attn.h
         self.hidden_size = model.encoders[0].self_attn.linear_out.out_features
 
-    
     def prepare_mask(self, mask):
         if len(mask.shape) == 2:
             mask = 1 - mask[:, None, None, :]
@@ -112,28 +111,6 @@ class ConformerEncoder(nn.Module):
 
         olens = mask.squeeze(1).sum(1)
         return xs_pad, olens
-
-    """
-    def forward(self,
-                speech: torch.Tensor,
-                speech_lengths: torch.Tensor,
-                ):
-        mask = self.make_pad_mask(speech_lengths)
-        mask = self.prepare_mask(mask)
-        if self.embed is None:
-            xs_pad = speech
-        else:
-            xs_pad = self.embed(speech)
-
-        encoder_outs = self.model.encoders(xs_pad, mask)
-        xs_pad, masks = encoder_outs[0], encoder_outs[1]
-
-        if isinstance(xs_pad, tuple):
-            xs_pad = xs_pad[0]
-        xs_pad = self.model.after_norm(xs_pad)
-
-        return xs_pad, speech_lengths
-    """
 
     def get_output_size(self):
         return self.model.encoders[0].size
